017c3393debafa806bd6552b7232c0122603b85e
D REDDY PRASAD <tech.dr.prasad@live.com>
2017-07-04 00:53:51 +0530

    video is playing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBh24w2uLNauhYn7JhPzvOPfGfYK2AB2f0';
 
-// create new component. This component should produce some HTML
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({ key: API_KEY, term: 'taylor swift' }, (videos) => {
+			this.setState({ videos });
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.videos[0]} />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
 // Take this component's generated code and put it in page
