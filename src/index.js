48c13da88c88dc15fe8fb4a964f35feed9256a97
D REDDY PRASAD <tech.dr.prasad@live.com>
2017-12-29 20:02:43 +0530

    I dont know what is about this commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
@@ -13,19 +14,32 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { videos: [] };
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
 
-		YTSearch({ key: API_KEY, term: 'taylor swift' }, (videos) => {
-			this.setState({ videos });
+		this.videoSearch('taylor swift');
+	}
+
+	videoSearch(term) {
+		YTSearch({ key: API_KEY, term: term }, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
 		});
 	}
 
 	render() {
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 400);
 		return (
 			<div>
-				<SearchBar />
-				<VideoDetail video={this.state.videos[0]} />
-				<VideoList videos={this.state.videos} />
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
 			</div>
 		);
 	}
